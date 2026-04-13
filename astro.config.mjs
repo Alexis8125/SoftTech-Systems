223080f1cce3f81dc342bef2549eda00e2ed9146
Alexis8125 <laarenass@ufpso.edu.co>
2026-04-13 15:25:53 -0500

    Fix styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://alexis8125.github.io',
-  base: '/SoftTech-Systems', // Cambia esto por el nombre de tu repositorio
+  site: 'https://softtechsys.site',
+  base: '/',
 });
